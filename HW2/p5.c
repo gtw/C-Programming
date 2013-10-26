c8a6854004afe1059d9ff5fb20c352ae4583e191
GT Wrobel <gtwrobel@gmail.com>
2013-10-26 16:17:48 -0500

    p5 almost done

--- HW2/p5.c
+++ HW2/p5.c
@@ -3,7 +3,7 @@
 #include<string.h>
 
 void alphabetize(char *text[], int nlines);
-int read_text(char **txtptr);
+int read_text(char ***txtptr);
 int word_comp(const void *a, const void *b);
 
 int main(int argc, char *argv[]){
@@ -11,60 +11,91 @@ int main(int argc, char *argv[]){
 	char **txtptr = (char**)malloc(sizeof(char*));
 	int len, i;
 
-	len = read_text(txtptr);
-		
+	len = read_text(&txtptr);
+/*
 	alphabetize(txtptr,len);
-	
+*/	
+
+
 	for(i=0;i<len;i++){
 		printf("%s\n",txtptr[i]);
 	}
+
 	return 0;
 }
 
-int read_text(char **txtptr){
+int read_text(char ***txtptr){
 
 	int line_count = 0;
 	int letter_count = 0;
 	int total_letters = 1;
 	int total_lines = 1;
-	
-	char c = getchar();
+	int newl_flag = 1;
+	char c;
 
-	while(c!=EOF){
-		
-		if(line_count >= total_lines){
-			txtptr = (char**)realloc(txtptr,sizeof(char*)*total_lines*2);
-			total_lines = total_lines * 2;
-		}
-		
-		txtptr[line_count] = (char*)malloc(sizeof(char));
-		total_letters = 1;
+	(*txtptr)[0] = (char*)malloc(sizeof(char));
 
-		while(c!='\n' && c != EOF){
-			printf("%c",c);
+	while((c=getchar())!=EOF){
+		
+		if(newl_flag == 0 && c != '\n'){
+			
 			if(letter_count >= total_letters){
-				txtptr[line_count] = (char*)realloc(txtptr[line_count],sizeof(char)*total_letters*2);
+				(*txtptr)[line_count]  = (char*)realloc((*txtptr)[line_count],sizeof(char)*total_letters*2);
 				total_letters = total_letters * 2;
 			}
-			txtptr[line_count][letter_count] = c;
+			
+			(*txtptr)[line_count][letter_count] = c;
+			letter_count++;	
+		}
+
+		else if(newl_flag == 0 && c == '\n'){
+			
+			if(letter_count >= total_letters){
+				(*txtptr)[line_count] = (char*)realloc((*txtptr)[line_count],sizeof(char)*(total_letters+1));
+				total_letters++;
+			}
+			
+			(*txtptr)[line_count][letter_count] = '\0';
+			letter_count = 0;
+			line_count++;
+			newl_flag = 1;
+			
+			if(line_count >= total_lines){
+				(*txtptr) = (char**)realloc((*txtptr),sizeof(char*)*total_lines*2);
+				total_lines = total_lines*2;
+			}
+			
+			(*txtptr)[line_count] = (char*)malloc(sizeof(char));
+			total_letters = 1;
+		}
+
+		else if(newl_flag == 1 && c != '\n'){
+
+			(*txtptr)[line_count][0] = c;
 			letter_count++;
-			c = getchar();
+			newl_flag = 0;
 		}
+
+		else if(newl_flag == 1 && c == '\n'){
+
+			continue;
 		
-				
+		}
+
+	}
+
+	if(newl_flag == 0){
 		if(letter_count >= total_letters){
-			txtptr[line_count] = (char*)realloc(txtptr[line_count],sizeof(char)*total_letters*2);
-			total_letters = total_letters * 2;
+			(*txtptr)[line_count] = (char*)realloc((*txtptr)[line_count],sizeof(char)*(total_letters+1));
+			total_letters = total_letters+1;
 		}
-		txtptr[line_count][letter_count] = '\0';
-		letter_count = 0;
+
+		(*txtptr)[line_count][letter_count] = '\0';
 		line_count++;
-		if(c!=EOF){
-			getchar();
-		}
 	}
 
 	return line_count;
+
 }
 
 void alphabetize(char *text[], int nlines){
@@ -75,8 +106,8 @@ void alphabetize(char *text[], int nlines){
 
 int word_comp(const void *a, const void *b){
         
-        char *ai = (char*)a;
-        char *bi = (char*)b;
+        char **ai = (char**)a;
+        char **bi = (char**)b;
 
-        return strcmp(ai,bi);
+        return strcmp(*ai,*bi);
 }
